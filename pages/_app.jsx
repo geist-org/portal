f7e6f4897a1674e5089cbc51226adb57f4d92b32
Ofek Ashery <ofek.ashery@gmail.com>
2020-04-26 18:43:56 +0300

    feat: redesign (#2)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,82 +2,68 @@ import Head from 'next/head'
 import React from 'react'
 import { ZEITUIProvider, useTheme, CSSBaseline } from '@zeit-ui/react'
 
+const myTheme = {
+  type: 'dark'
+}
+
 const Application = ({ Component, pageProps }) => {
   const theme = useTheme()
-  
+
   return (
     <>
-    <Head>
-      <title>ZEIT UI</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="dns-prefetch" href="//zeit-ui.co" />
-      <meta name="google" value="notranslate" />
-      <meta name="referrer" content="strict-origin" />
-      <meta name="description" content="An unofficial organization for zeit-ui." />
-      <meta property="og:site_name" content="zeit-ui" />
-      <meta property="og:description" content="An unofficial organization for zeit-ui." />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="author" content="zeit-ui" />
-      <meta name="twitter:creator" content="@echo_witt" />
-      <meta property="og:title" content="zeit-ui" />
-      <meta property="og:url" content="//zeit-ui.co" />
-      <meta property="og:image" content="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png" />
-      <meta property="twitter:image" content="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png" />
-      <meta name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1, viewport-fit=cover" />
-    </Head>
-    <ZEITUIProvider>
-      <CSSBaseline>
-        <Component {...pageProps} />
-      </CSSBaseline>
-      <style global jsx>{`
-        .tag {
-          color: ${theme.palette.accents_5};
-        }
-        
-        .punctuation {
-          color: ${theme.palette.accents_5};
-        }
-        
-        .attr-name {
-          color: ${theme.palette.accents_6};
-        }
-        
-        .attr-value {
-          color: ${theme.palette.accents_4};
-        }
-        
-        .language-javascript {
-          color: ${theme.palette.accents_4};
-        }
-        
-        span[class*="class-name"] {
-          color: ${theme.palette.purple};
-        }
-        
-        span.token.string {
-          color: ${theme.palette.accents_5};
-        }
-        
-        span.keyword {
-          color: ${theme.palette.success}
-        }
-        
-        span.plain-text {
-          color: ${theme.palette.accents_3};
-        }
-        
-        html, body {
-          overflow-x: hidden;
-        }
-        
-        @media only screen and (max-width: 767px) {
-          html {
-            font-size: 15px;
+      <Head>
+        <title>ZEIT UI</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="dns-prefetch" href="//zeit-ui.co" />
+        <meta name="google" value="notranslate" />
+        <meta name="referrer" content="strict-origin" />
+        <meta name="description" content="Implementations of Vercel Design." />
+        <meta property="og:site_name" content="zeit-ui" />
+        <meta property="og:description" content="Implementations of Vercel Design." />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="author" content="zeit-ui" />
+        <meta name="twitter:creator" content="@echo_witt" />
+        <meta property="og:title" content="zeit-ui" />
+        <meta property="og:url" content="//zeit-ui.co" />
+        <meta
+          property="og:image"
+          content="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png"
+        />
+        <meta
+          property="twitter:image"
+          content="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png"
+        />
+        <meta
+          name="viewport"
+          content="initial-scale=1, maximum-scale=1, minimum-scale=1, viewport-fit=cover"
+        />
+      </Head>
+      <ZEITUIProvider theme={myTheme}>
+        <CSSBaseline>
+          <main>
+            <Component {...pageProps} />
+          </main>
+        </CSSBaseline>
+        <style global jsx>{`
+          main {
+            max-width: 600pt;
+            margin: 0 auto;
+            padding: ${theme.layout.pageMargin};
           }
-        }
-      `}</style>
-    </ZEITUIProvider>
+
+          html,
+          body {
+            overflow-x: hidden;
+          }
+
+          @media only screen and (max-width: ${theme.layout.breakpointMobile}) {
+            html {
+              font-size: 15px;
+            }
+          }
+        `}</style>
+      </ZEITUIProvider>
     </>
   )
 }
---
+++ pages/index.jsx
@@ -0,0 +1,131 @@
+import { Text, useTheme, Link, Spacer, Row, Col, Code, Avatar, Dot } from '@zeit-ui/react'
+
+const Home = () => {
+  const theme = useTheme()
+
+  return (
+    <main>
+      <Spacer y={3} />
+      <Text h2>Vercel UI</Text>
+      <p>
+        Hello 👋
+        <br />
+        We are an{' '}
+        <Link color pure href="https://github.com/zeit-ui">
+          open source
+        </Link>{' '}
+        UI Organization and big fans of{' '}
+        <Link color href="https://vercel.com/design">
+          Vercel Design
+        </Link>{' '}
+        style.
+        <br />
+        Here are some of our implementation of Vercel Design, so you can use them well.
+      </p>
+
+      <Link pure href="https://react.zeit-ui.co" target="_blank">
+        <Code>zeit-ui/react</Code>&nbsp;- React UI library
+      </Link>
+      <Spacer y={0.5} />
+      <Link pure href="https://vue.zeit-ui.co" target="_blank">
+        <Code>zeit-ui/vue</Code>&nbsp;- Vue UI library
+      </Link>
+      <Spacer y={0.5} />
+      <Link pure href="https://typography.zeit-ui.co" target="_blank">
+        <Code>typography</Code>&nbsp;- Typography styles
+      </Link>
+      <Spacer y={0.5} />
+      <Link pure href="https://zeit-style.now.sh" target="_blank">
+        <Code>styles</Code>&nbsp;- CSS styles
+      </Link>
+
+      <Spacer y={3} />
+      <Dot type="success">
+        <Text b type="success">
+          Contributors
+        </Text>
+      </Dot>
+      <Spacer y={0.7} />
+
+      <Row>
+        <Col offset=".5">
+          <Avatar.Group>
+            <Link pure href="https://github.com/unix" target="_blank">
+              <Avatar
+                stacked
+                draggable="false"
+                src="https://avatars3.githubusercontent.com/u/11304944?s=96&v=4"
+              />
+            </Link>
+            <Link pure href="https://github.com/ofekashery" target="_blank">
+              <Avatar
+                stacked
+                draggable="false"
+                src="https://avatars2.githubusercontent.com/u/16443111?s=96&v=4"
+              />
+            </Link>
+            <Link pure href="https://github.com/l1shen" target="_blank">
+              <Avatar
+                stacked
+                draggable="false"
+                src="https://avatars2.githubusercontent.com/u/8299540?s=96&v=4"
+              />
+            </Link>
+            <Link pure href="https://github.com/Lzumikonata" target="_blank">
+              <Avatar
+                stacked
+                draggable="false"
+                src="https://avatars2.githubusercontent.com/u/17999142?s=96&v=4"
+              />
+            </Link>
+          </Avatar.Group>
+        </Col>
+      </Row>
+
+      <style global jsx>{`
+        h1 {
+          font-size: 2em !important;
+          font-weight: 700;
+        }
+
+        .tag {
+          color: ${theme.palette.accents_5};
+        }
+
+        .punctuation {
+          color: ${theme.palette.accents_5};
+        }
+
+        .attr-name {
+          color: ${theme.palette.accents_6};
+        }
+
+        .attr-value {
+          color: ${theme.palette.accents_4};
+        }
+
+        .language-javascript {
+          color: ${theme.palette.accents_4};
+        }
+
+        span[class*='class-name'] {
+          color: ${theme.palette.purple};
+        }
+
+        span.token.string {
+          color: ${theme.palette.accents_5};
+        }
+
+        span.keyword {
+          color: ${theme.palette.success};
+        }
+
+        span.plain-text {
+          color: ${theme.palette.accents_3};
+        }
+      `}</style>
+    </main>
+  )
+}
+
+export default Home
